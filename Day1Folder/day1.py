be15ea340f8f3a8eb0694b11df8d75900ab3abd5
xinyuu24 <xzhang.angela@gmail.com>
2025-12-16 06:30:53 +0000

    Dec 16, 2025, 6:30 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -67,3 +67,15 @@ def division(number1 , number2):
 division (12,9)
 ########################################################################
 # Additional exercises:
+# Rearrange the lines of code below such that the output of the program is:
+
+## Hey
+## This is really simple but important
+## Python reads from the top to the bottom
+## Bye
+
+print("Hey")
+print("Bye")
+print("This is really simple but important")
+print("Python reads from the top to the bottom")
+
