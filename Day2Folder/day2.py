5d7acff83640d365e26cc24cd6283c010e80824d
xinyuu24 <xzhang.angela@gmail.com>
2025-12-16 07:08:27 +0000

    Dec 16, 2025, 7:08 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -40,7 +40,7 @@ for letter in "idle":
 
 # Write a program to New Year Eve CountDown Program
 # count down from 10 to 0 and say "Happy New Year!" at the end
-for count in range(10):
+for count in range(11):
     print(count)
 print("happy near year")
 # Exercise 2:
