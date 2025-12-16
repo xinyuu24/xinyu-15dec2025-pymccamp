338e175e781ab273dd68497bcddf91090785d88a
xinyuu24 <xzhang.angela@gmail.com>
2025-12-16 06:53:39 +0000

    Dec 16, 2025, 6:53 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -90,6 +90,6 @@ goodbye()
 ## Increase the value of variable A by 2.5 using the + operator
 ## Double the value of variable B using the * operator
 ## Print out the sum of variable A and B
-vable = "10"
+variable = "10"
 
 
