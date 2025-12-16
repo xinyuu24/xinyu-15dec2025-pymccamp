5a76a1769331723990e7361184d07fb2088ff6f5
xinyuu24 <xzhang.angela@gmail.com>
2025-12-16 07:21:21 +0000

    Dec 16, 2025, 7:21 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -58,7 +58,7 @@ print("happy new year")
 
 # Count all the multiples of 3 using the for loop, from 3 to 36
 
-#for number in range(13):
+#for number in range(3,):
 #  print (number)
 
 ##############################################################################
