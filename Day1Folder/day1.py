6a977c02a03df373a159d290d7eab24820842c5f
xinyuu24 <xzhang.angela@gmail.com>
2025-12-16 06:51:24 +0000

    Dec 16, 2025, 6:51 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -85,6 +85,10 @@ def goodbye():
   print("today is so eventful!")
   print("good bye! see u next time!")
 goodbye()
-
+# Exercise 5
+# For you to do:
+## Increase the value of variable A by 2.5 using the + operator
+## Double the value of variable B using the * operator
+## Print out the sum of variable A and B
 
 
