43c1a4a2bf0de9ba227e4a424e7f43505623e1f8
xinyuu24 <xzhang.angela@gmail.com>
2025-12-16 07:45:43 +0000

    Dec 16, 2025, 7:45 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -55,10 +55,6 @@ for count in range(10,0,-1):
     print(count)
 print("happy new year")
 # Exercise 2:
-
-# Write a program that sings the nursery rhymes using Loops and Nested Loops (e.g. Loop in a Loop)
-
-# Example: Baby Shark, Mary had a little lamb
 animal = "little lamb"
 
 for count in range(2):
