7b0bd03da1a5c1a0386bbaa53bfb8f2449348cd4
xinyuu24 <xzhang.angela@gmail.com>
2025-12-16 08:28:13 +0000

    Dec 16, 2025, 8:28 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -66,5 +66,5 @@ for number in range(3,37,3):
 # Exercise 4:
 
 # Using a for loop Say out all the odd numbers from 1 to 99
-for number in range(0,100,2):
+for number in range(1,100,2):
     print(number)
