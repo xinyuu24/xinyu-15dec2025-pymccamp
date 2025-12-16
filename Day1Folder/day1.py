0d8c2a1d852d201c00fa1edc906303232454d5ba
xinyuu24 <xzhang.angela@gmail.com>
2025-12-16 05:59:51 +0000

    Dec 16, 2025, 5:59 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,41 +1,71 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
-
 ########################################################################
 # Task 1:
-
-
-
+print("hello from day1")
 ########################################################################
 # Task 2:
-
-
-
+print("i am xinyu")
+print("i am 13 years old")
+print("i am from CHIJ St.Nicholas Girls School")
+print("i am currently staying in Bishan")
+print("if i have $2000, i will save half and use the other half to eat and shop")
 ########################################################################
 # Task 3:
-
-
-
+zoo1 = "panda"
+zoo2 = "butterfly"
+zoo3 = "jellyfish"
+print(zoo1)
+print(zoo2)
+print(zoo3)
 ########################################################################
 # Task 4:
-
-
-
+var1 = 10
+var2 = 2
+ans1 = var1 + var2
+print(ans1)
+ans2 = var1 - var2
+print(ans2)
+ans3 = var1 * var2
+print(ans3)
+ans4 = var1 / var2
+print(ans4)
 ########################################################################
 # Task 5:
-
-
+name = "snoppy"
+hobby = "play snow" 
+print("my name is " + name)
+print("i like to " + hobby)
 
 ########################################################################
 # Task 6:
-
-
-
+def greetings():
+    print("hello")
+    print("my name is xinyu")
+    print("i love to eat")
+greetings() # call a function
+greetings() # call a function
+def intro():
+    print("i am xinyu")
+    print("i am 13 years old")
+    print("i am from CHIJ St.Nicholas Girls School")
+    print("i am currently staying in Bishan")
+    print("if i have $2000, i will save half and use the other half to eat and shop")
+intro()
 ########################################################################
 # Task 7:
 
+# Task 7: Python Functions with Parameters
+## Continue the below in day1.py
 
+# Write a function called **addition()** that will calculate the addition of 2 numbers. 
+# When you define the function, add in 2 parameters **number1** and **number2**
+
+# In the function, add number1 and number 2
+# print the result out
+number1 = 3
+number2 = 24
+def addition("number1" + "number2"):
 
 ########################################################################
 # Additional exercises:
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -1,6 +1,22 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
 ################# Function Section #############################
+def addition(num1,num2):
+    player.say(num1*num2)
 
+addition(12,9)
+
+def teleport():
+    agent.teleport_to_player()
+player.on_chat("come", teleport)
+
+def tl():
+    agent.turn(LEFT)
+player.on_chat("tl",tl)
+
+
+def tr ():
+    agent.turn(RIGHT)
+player.on_chat("tr",tr)
 
 ################## On Chat Commands Section #####################
