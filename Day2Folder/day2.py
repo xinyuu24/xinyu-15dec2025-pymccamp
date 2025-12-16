2cc4719f09f0b595726a1aeb8da2e1dd9b7d39e9
xinyuu24 <xzhang.angela@gmail.com>
2025-12-16 07:39:48 +0000

    Dec 16, 2025, 7:39 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -66,6 +66,7 @@ for count in range(2):
     for count in range(3):
         print(animal)
     print("mary had a little lamb")
+    print
 
 ##############################################################################
 # Exercise 3:
