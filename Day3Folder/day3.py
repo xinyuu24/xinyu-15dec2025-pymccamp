d4a7e7eea0473811faa91a0bf8b05a72dc503feb
xinyuu24 <xzhang.angela@gmail.com>
2025-12-18 07:14:11 +0000

    Dec 18, 2025, 7:14 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -4,8 +4,10 @@ print("hello from day3")
 
 ########################################################################
 # Task 1:
-
-
+# name = input("what is your name?")
+# title = input("what is your title?")
+# task = input("what is the task?")
+# print(title, name, "orders the peasants to", task)
 
 ########################################################################
 # Task 2:
@@ -14,9 +16,10 @@ print("hello from day3")
 
 ########################################################################
 # Task 3:
-
-
-
+# num1 = input("what is the first number? ")
+# num2 = input("what is the second number? ")
+# ans = int(num1) + int(num2)
+# print("the answer is", ans)
 ########################################################################
 # Task 4:
 
@@ -24,12 +27,21 @@ print("hello from day3")
 
 ########################################################################
 # Task 5:
-
-
+# hidden_password = "passme"
+# guess = input("do you know my password")
+# if hidden_password == guess:
+#     print("please come in")
+# else:
+#     print("go away or i will call the police")
 
 ########################################################################
 # Task 6:
-
+# legalage = "21"
+# yourage = input("what is your age")
+# if legalage <= yourage:
+#     print("you are legal to vote for the next president")
+# else:
+#     print("you are not old enough to vote yet")
 
 
 ########################################################################
@@ -44,3 +56,41 @@ print("hello from day3")
 
 ########################################################################
 # Additional exercises:
+# Exercise 1
+
+# Write a program to ask the user to enter a password.
+
+# If the password is correct, say "Access Granted"
+
+# Else, say "Access Denied"
+
+# The user is given a change to enter the password 3 times until the correct password is given.
+# If the user fails the 3rd attempt, the program will say "System Locked. I call police."
+
+
+
+hidden_password = "passme"
+tries =3
+guess = input("password?")
+for count in range(tries):
+    if hidden_password == guess:
+        print("access granted")
+        break
+    else:
+        print("access denied")
+        if count == tries - 1:
+            print("System Locked. I call police")
+
+
+is_correct = False
+for count in range(3):
+    guess = input("do you know my password")
+
+    if hidden_password == guess:
+        print("please come in")
+        is_correct = True
+        break
+    else:print("its okay. try again")
+
+if is_correct == False:
+    print("your account is locked")
--- Day3Folder/minecraft_day3.py
+++ Day3Folder/minecraft_day3.py
@@ -1,2 +1,33 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def chop(height):
+    for count in range(height):
+        agent.destroy(UP)
+        agent.destroy(FORWARD)
+        agent.collect_all()
+        agent.move(UP, 1)
+    agent.move(DOWN, height)
+player.on_chat("ch", chop)
+
+def stone():
+    for count in range(30):
+        agent.destroy(LEFT)
+        agent.destroy(RIGHT)
+        agent.destroy(DOWN)
+        agent.collect_all()
+        agent.move(FORWARD, 1)
+player.on_chat("mine", stone)
+
+def stone2():
+    for count in range(30):
+        agent.destroy(UP)
+        agent.destroy(DOWN)
+        agent.collect_all()
+        agent.move(FORWARD, 1)
+player.on_chat("minef", stone2)
+
+def build():
+    for count in range(31):
+        agent.place(FORWARD)
+        agent.move(RIGHT, 1)
+player.on_chat("build", build)
